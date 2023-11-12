4813d963ca943e7b3f1b0a626f48a77f227a5e46
Rich Hamburg <github@rhamburg.com>
2023-11-12 17:41:20 -0500

    Fix analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'static',
 	adapter: vercel({
 		webAnalytics: {
 		  enabled: true,
