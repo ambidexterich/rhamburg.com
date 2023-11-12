c66b655095d79568919508f61e1fd168ec8a1395
Rich Hamburg <github@rhamburg.com>
2023-11-11 20:51:40 -0500

    initial astro build

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
+export default defineConfig({
+	adapter: vercel({
+		webAnalytics: {
+		  enabled: true,
+		},
+		speedInsights: {
+			enabled: true,
+		  },
+	  }),
+});
