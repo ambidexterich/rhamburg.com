dba9f0029981113beec7e8a748dfc02e78c9f78e
Rich Hamburg <github@rhamburg.com>
2026-03-01 14:50:53 -0500

    chore(setup): add EditorConfig, ESLint, and Prettier configurations; remove Biome settings

---
+++ www/eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import json from '@eslint/json';
+import { defineConfig } from 'eslint/config';
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default defineConfig([
+	{
+		ignores: ['**/dist', '**/node_modules', '**/.astro', '**/.github', '**/.changeset'],
+	},
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		plugins: { js },
+		extends: ['js/recommended'],
+		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+	},
+	...tseslint.configs.recommended,
+	{
+		files: ['**/*.json'],
+		plugins: { json },
+		language: 'json/json',
+		extends: ['json/recommended'],
+	},
+	{
+		files: ['**/*.jsonc'],
+		plugins: { json },
+		language: 'json/jsonc',
+		extends: ['json/recommended'],
+	},
+	...eslintPluginAstro.configs.recommended,
+	{
+		rules: {
+			// override/add rules settings here, such as:
+			// "astro/no-set-html-directive": "error"
+		},
+	},
+]);
